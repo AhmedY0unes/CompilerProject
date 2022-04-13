f2adf2ff76b38f70650bff4e0bcf1402d4fc83c2
ahmed434 <ahmed170152@feng.bu.edu.eg>
2022-04-13 21:55:11 +0200

    first task

---
+++ Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main(String[] args) {
+        int x = 0;
+        int y;
+        if(x==0){
+            y=10;
+        }
+        else{
+            if(true){
+                x++;
+            }
+            y=10;
+        }
+        if(true){
+            y++;
+        }
+        int a[] = {1,2,3};
+        for(int i = 0 ; i < 1 ; i++){
+            int w;
+            int z;
+        }
+    }
+}
