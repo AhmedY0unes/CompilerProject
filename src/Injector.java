bb81e22daba2a525fd5c82e00e82d97778c08b7f
mohamed-el-sharkawy <melsharkawy346@gmail.com>
2022-05-01 23:35:22 +0200

    handle the case of statement without { and all and handle spaces and more statement

--- src/Injector.java
+++ src/Injector.java
@@ -11,13 +11,16 @@ public class Injector{
         CommonTokenStream tokens = new CommonTokenStream(lex);
 
         JavaParser parser = new JavaParser(tokens);
-        String allowedTokens[] = {"if" , "while" , "for" , "do" , "try" , "catch" , "case", "class" , "else" , "default"};
+        String allowedTokens[] = {"if" , "while" , "for" , "do" , "try" , "catch" , "case", "else" , "default"};
         String texts = "";
         TokenStreamRewriter r = new TokenStreamRewriter(tokens);
         parser.compilationUnit();
         String tokenText = "";
-        r.insertBefore(0, "package output;");
-        int c = 0;
+        r.insertBefore(0, "import java.io.FileWriter;\n");
+        r.insertBefore(0, "import java.io.IOException;\n");
+        r.insertBefore(0, "import java.io.File;\n");
+        r.insertBefore(0, "package output;\n");
+        int c = 0 , j = 0;
         boolean thereIsADo = false;
         for (int i = 0 ; i < tokens.size()-1; i++ ) {
             System.out.println(tokens.get(i).getText());
@@ -26,6 +29,7 @@ public class Injector{
                     tokenText = tokens.get(i).getText();
                 }
             }
+
             if(tokenText.equals("do")){
                 thereIsADo = true;
             }
@@ -33,11 +37,15 @@ public class Injector{
                 tokenText = "";
                 continue;
             }
+            if((tokens.get(i).getText().equals("for") || tokens.get(i).getText().equals("while") || tokens.get(i).getText().equals("do")))
+                {
+                    r.insertBefore(i, "boolean f" + j + "  = false;\n");
+                }
             if(tokens.get(i).getText().equals(":") && (tokenText.equals("case") || tokenText.equals("default"))){
                 r.insertAfter(i, "\n System.out.println(\"" + "Block" + ++c + "\"); \n");
                 tokenText = "";
             }
-            if(tokens.get(i).getText().equals("{") && (tokens.equals("class") || tokenText.equals("else"))){
+            if(tokens.get(i).getText().equals("{") && (tokenText.equals("class") ||tokenText.equals("do") || tokenText.equals("else"))){
                 r.insertAfter(i, "\n System.out.println(\"" + "Block" + ++c + "\"); \n");
                 tokenText = "";
             }
@@ -47,10 +55,32 @@ public class Injector{
                     oneAfter = tokens.get(++i).getText();
                 }
                 if(oneAfter.equals("{")){
-                    r.insertAfter(i + 1, "\n System.out.println(\"" + "Block" + ++c + "\"); \n");
+                    if(tokenText.equals("for") || tokenText.equals("while") || tokenText.equals("do")){
+                        r.insertAfter(i + 1, "if(!f" + j +"){\nSystem.out.println(\"" + "Block" + ++c + "\"); f" + j +" = true;} \n");
+                        j++;
+                    }
+                    else r.insertAfter(i + 1, "\n System.out.println(\"" + "Block" + ++c + "\"); \n");
                 }
                 else{
-                    r.insertAfter(i, "\n System.out.println(\"" + "Block" + ++c + "\"); \n");
+                    if(tokenText.equals("for") || tokenText.equals("while")){
+                        r.insertAfter(i - 1, "{if(!f" + j +"){\nSystem.out.println(\"" + "Block" + ++c + "\"); f" + j +" = true;} \n");
+                        j++;
+                        while(i++ < tokens.size() - 1){
+                            if(tokens.get(i).getText().equals(";")){
+                                r.insertAfter(i, "}\n");
+                                break;
+                            }
+                        }
+                    }
+                    else {
+                        r.insertAfter(i - 1, "{\ntry { FileWriter myWriter = new FileWriter(\"./output/output.txt\",true); myWriter.write(\"Block"+ ++c + "\"); myWriter.close();} catch (IOException e) {e.printStackTrace();}\n");
+                        while(i++ < tokens.size() - 1){
+                            if(tokens.get(i).getText().equals(";")){
+                                r.insertAfter(i, "}\n");
+                                break;
+                            }
+                        }
+                    }
                 }
                 tokenText = "";
             }
@@ -66,20 +96,18 @@ public class Injector{
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String inputPath = sc.nextLine();
-        System.out.println(inputPath);
         boolean f = false;
         String post = inputPath.substring(inputPath.lastIndexOf('/'));
         String initPre = inputPath.substring(0 , inputPath.lastIndexOf('/'));
         String pre = initPre.substring(0, initPre.lastIndexOf('/'));
         String outputPath = pre + "/output" + post;
         String x = injector(inputPath);
-        System.out.println(x);
 
             BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath));
             writer.write(x);
 
             writer.close();
 
+        }
     }
-}
 ///home/mohamed/CompilerProject/src/Test.java
