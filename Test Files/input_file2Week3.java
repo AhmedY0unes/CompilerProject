0cda07fe8ec7c6744f3bed0b829867e870ebfa5c
Ahmed Younes <ahm3dy0un3s@gmail.com>
2022-05-04 01:15:22 +0200

    Add files for ReadMe

---
+++ Test Files/input_file2Week3.java
@@ -0,0 +1,23 @@
+public class Test2 {
+    public static void print(){
+        System.out.println("Shekabala Shekabala Shekabala");
+    }
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 10;
+        while (++x < 22)
+            System.out.println("Abo Treka");
+
+        while (x < 85){
+            for (int i=0; i < 45; i++)
+                System.out.println("EL Qadya momkon");
+            x++;
+        }
+        do{
+            System.out.println("I want to graduate");
+        }while (y++ < 20);
+        for(int i = 80; i < 60; i++){
+            print();
+        }
+    }
+}
