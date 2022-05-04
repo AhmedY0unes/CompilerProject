2776ea1d90b864d242c5f3a4521b6de1ace5789c
unknown <ahmed.hesham_1999@yahoo.com>
2022-05-04 08:18:37 +0200

    Make Automatic Pipline and make Doxygen Docs

---
+++ doc/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['htmlinjector_0',['htmlInjector',['../class_injector.html#a63def0127d146ec8c56b487a0efcdb96',1,'Injector']]]
+];
---
+++ doc/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['injector_0',['injector',['../class_injector.html#ad407dfbe9e5b195321f25e0d27cb344c',1,'Injector']]]
+];
